09bb0f11446a23576985fc572007cbb69eda0bd3
tobecoder007 <133747156+tobecoder007@users.noreply.github.com>
2023-10-24 14:18:49 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -15,35 +15,35 @@ const pizzaData = [
     name: "Pizza Margherita",
     ingredients: "Tomato and mozarella",
     price: 10,
-    photoName: "pizzas/margherita.jpg",
+    photoName: "Pizzas/margherita.jpg",
     soldOut: false,
   },
   {
     name: "Pizza Spinaci",
     ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
     price: 12,
-    photoName: "pizzas/spinaci.jpg",
+    photoName: "Pizzas/spinaci.jpg",
     soldOut: false,
   },
   {
     name: "Pizza Funghi",
     ingredients: "Tomato, mozarella, mushrooms, and onion",
     price: 12,
-    photoName: "pizzas/funghi.jpg",
+    photoName: "Pizzas/funghi.jpg",
     soldOut: false,
   },
   {
     name: "Pizza Salamino",
     ingredients: "Tomato, mozarella, and pepperoni",
     price: 15,
-    photoName: "pizzas/salamino.jpg",
+    photoName: "Pizzas/salamino.jpg",
     soldOut: true,
   },
   {
     name: "Pizza Prosciutto",
     ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
     price: 18,
-    photoName: "pizzas/prosciutto.jpg",
+    photoName: "Pizzas/prosciutto.jpg",
     soldOut: false,
   },
 ];
@@ -94,9 +94,20 @@ function Footer() {
   const hour = new Date().getHours()
   const openHour = 12;
   const closeHour = 22;
+  const isOpen = hour >= openHour && hour <= closeHour;
+  console.log(isOpen);
   // if (hour >= openHour && hour <= closeHour) alert("We're currently open");
   // else alert("Sorry we're closed") 
-  return <footer className='footer'>{new Date().toLocaleTimeString()} We're currently open</footer>
+  return <footer className='footer'>
+    {isOpen && (
+      <div className='order'>
+        <p>
+          We're open until {closeHour}:00. Come visit us or oder online.
+        </p>
+        <button className='btn'>Order</button>
+      </div>
+    )}
+  </footer>
 }
 
 
